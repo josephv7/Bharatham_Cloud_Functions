a3b968a02b801735c3c5e5352a936e0e52cad224
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-11-07 16:14:16 +0530

    added function, need to resolve error

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,16 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const updatesRef = admin.database().ref('main/events');
+exports.pushTransaction = functions.updatesRef.ref('/main/events/{event_name}/{position}/{id}/').onCreate((snapshot, context) => {
+
+
+const score = snapshot.score.val();
+const house = snapshot.house.val();
+const scoreAdd = admin.database().ref('score').child(house);
+	
+return scoreAdd.transaction(current)=>{
+	return (current||0)+score;
+
+});
