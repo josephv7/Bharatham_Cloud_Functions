e7e40006182afc5d6c3954b01c7256e62700386f
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-11-07 16:30:16 +0530

    deployed

--- functions/index.js
+++ functions/index.js
@@ -2,15 +2,17 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-const updatesRef = admin.database().ref('main/events');
-exports.pushTransaction = functions.updatesRef.ref('/main/events/{event_name}/{position}/{id}/').onCreate((snapshot, context) => {
+const database = admin.database().ref('main/');
+exports.pushTransaction = functions.database.ref('main/events/{event_name}/{position}/{id}/').onCreate((snapshot, context) => {
 
 
 const score = snapshot.score.val();
 const house = snapshot.house.val();
 const scoreAdd = admin.database().ref('score').child(house);
 	
-return scoreAdd.transaction(current)=>{
+return scoreAdd.transaction((current)=>{
 	return (current||0)+score;
+
+});
 
 });
